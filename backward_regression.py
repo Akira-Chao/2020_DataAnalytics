9e6b27c85bbd0c54868c92778b392bb427544720
Ricardo S. Chao <richiechao95@gmail.com>
2020-04-10 14:09:16 +0800

    [new] forward, backward regression.py

---
+++ backward_regression.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+
+
+def backward_regression(X, y,
+                           threshold_out,
+                           verbose=False):
+    print('Backward Selection')
+    included=list(X.columns)
+    while True:
+        changed=False
+        model = sm.OLS(y, sm.add_constant(pd.DataFrame(X[included]))).fit()
+        # use all coefs except intercept
+        pvalues = model.pvalues.iloc[1:]
+        worst_pval = pvalues.max() # null if pvalues is empty
+        if worst_pval > threshold_out:
+            changed=True
+            worst_feature = pvalues.idxmax()
+            included.remove(worst_feature)
+            if verbose:
+                print('Drop {:30} with p-value {:.6}'.format(worst_feature, worst_pval))
+        if not changed:
+            break
+    return included
